d9bc58d64608e5c36fb14eaaa7ff160730244806
Lukas Reim <lukasr101@gmail.com>
2021-10-19 17:03:50 +0200

    adding logging of errors in mocros.h

--- includes/macros.h
+++ includes/macros.h
@@ -17,26 +17,43 @@ exit(-1);\
 
 #ifdef DEBUG
 #define LOGF(fmt, ...) \
-do { fprintf(this->logFile, "\t" fmt "\n", __VA_ARGS__); } while (0)
+do { fprintf(this->logFile, "----" fmt "\n", __VA_ARGS__); } while (0)
 
 #define LOG(text) \
 do { fprintf(this->logFile, "\t" text "\n"); } while (0)
+
+#define LOGE(text) \
+do { fprintf(this->logFile, "ERROR:" text "\n"); } while (0)
+
 #else
 #define LOGF(fmt, ...)
 #define LOG(text)
+#define LOGE(text)
 #endif
 
 #ifdef DEBUG_METHODS
 #define LOGM() \
-do { fprintf(this->logFile, "%s:%d:%s()\n", __FILE__, \
-__LINE__, __func__); } while (0)
+do { fprintf(this->logFile, "Function:%s()\nFile:%s:%d\n",__func__, __FILE__, \
+__LINE__); } while (0)
 #else
 #define LOGM()
 #endif
 
 #ifdef DEBUG_RETURN_VALUES
 #define RETURN(ret) \
-fprintf(this->logFile, "%s() returned %d\n", __func__, ret); return ret;
+if (ret < 0) {      \
+    char* text;    \
+    switch(ret) {   \
+        case -2:  text = "No such file or directory"; break; \
+        case -22:  text = "Invalid argument"; break; \
+        case -9:  text = "File not open"; break; \
+        case -28:  text = "To many files in directory"; break; \
+        case -18:  text = "File already exists"; break;      \
+        default: text = "ERROR"; break;                \
+    }               \
+    fprintf(this->logFile, "ERROR with code:'%d' : %s", ret, text);                 \
+}        \
+fprintf(this->logFile, "%s() returned %d\n\n------------------------\n", __func__, ret); return ret;
 #else
 #define RETURN(ret) return ret;
 #endif
