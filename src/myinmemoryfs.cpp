3ffa1bbe86b80b33aa9305fee9876dbd2d019bf2
Lukas <Lukasr101@gmail.com>
2021-10-27 15:16:53 +0200

    delete %s

--- src/myinmemoryfs.cpp
+++ src/myinmemoryfs.cpp
@@ -406,7 +406,7 @@ void* MyInMemoryFS::fuseInit(struct fuse_conn_info *conn) {
 /// This function is called when the file system is unmounted. You may add some cleanup code here.
 void MyInMemoryFS::fuseDestroy() {
     LOGM();
-    LOG("Delete files from file map%s");
+    LOG("Delete files from file map");
     for (auto const& item : files) {
         delete item.second;
     }
