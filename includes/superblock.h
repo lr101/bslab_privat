4427cf613e0c0f9b5051dc362a17b602bfad6347
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2021-11-11 18:20:19 +0100

    cleanup superblock class

--- includes/superblock.h
+++ includes/superblock.h
@@ -2,25 +2,30 @@
 // Created by lukas on 11.11.2021.
 //
 #pragma once
+
 #include <math.h>
+#include <cstdint>
 
 #include "myfs-structs.h"
 
 #define BYTE_SIZE 8
-#define SUPERBLOCK_SIZE
+#define SUPERBLOCK_SIZE 1
+#define D_MAP_INDEX 1
+
 typedef uint32_t index_t;
 
 class Superblock {
+
     size_t size; //blocks
-    index_t d_map_index;
     index_t i_map_index;
-    index_t inode_index;
+    index_t i_node_index;
     index_t data_index;
+
 public:
-    Superblock(size_t size, size_t inode_num);
+    Superblock(size_t size, size_t i_node_num);
+    ~Superblock();
 
-    size_t cal_imap_size(size_t inode_num);
-
-    size_t cal_dmap_size (size_t size)
+    size_t cal_imap_size(size_t i_node_num);
+    size_t cal_dmap_size (size_t size);
 };
 
