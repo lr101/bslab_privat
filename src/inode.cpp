a5cdfec323886b6c744fca65775cdc02fcf3fed1
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2021-11-22 18:15:00 +0100

    added blockdevice errno to function output

--- src/inode.cpp
+++ src/inode.cpp
@@ -248,18 +248,24 @@ int Inode::getBlockList(InodePointer* inodePtr, off_t size, off_t offset, std::v
 
     for (uint32_t i = startBlockIndex; i <= endBlockIndex; i++) {
         if (i < DIR_BLOCK) {
+
             blockList->push_back(this->block[i]);
+
         } else if (i -= DIR_BLOCK < IND_BLOCK * N_BLOCK_PTR) {
+
             char *buffer = new char[BLOCK_SIZE];
-            inodePtr->blockDevice->read(this->block[(i >> BLOCK_PTR_BITS) + DIR_BLOCK], buffer);
+            ret += inodePtr->blockDevice->read(this->block[(i >> BLOCK_PTR_BITS) + DIR_BLOCK], buffer);
             blockList->push_back(*(uint32_t *) (buffer[i & BLOCK_PTR_BIT_MASK]));
             delete[] buffer;
+
         } else if (i -= IND_BLOCK * N_BLOCK_PTR < DIND_BLOCK * N_BLOCK_PTR * N_BLOCK_PTR) {
+
             char *buffer = new char[BLOCK_SIZE];
-            inodePtr->blockDevice->read(this->block[(i >> BLOCK_PTR_BITS * 2) + DIR_BLOCK + IND_BLOCK], buffer);
-            inodePtr->blockDevice->read(*(uint32_t *) (buffer[i >> BLOCK_PTR_BITS & BLOCK_PTR_BIT_MASK]), buffer);
+            ret += inodePtr->blockDevice->read(this->block[(i >> BLOCK_PTR_BITS * 2) + DIR_BLOCK + IND_BLOCK], buffer);
+            ret += inodePtr->blockDevice->read(*(uint32_t *) (buffer[i >> BLOCK_PTR_BITS & BLOCK_PTR_BIT_MASK]), buffer);
             blockList->push_back(*(uint32_t *) (buffer[i & BLOCK_PTR_BIT_MASK]));
             delete[] buffer;
+
         } else {
             ret = -EINVAL;
         }
